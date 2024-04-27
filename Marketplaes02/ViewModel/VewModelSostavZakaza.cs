add2f4d6ab649ecb39d1726dac63764b1104d7ae
Admin <Admin@DESKTOP-V6V9F0T>
2024-04-28 00:11:02 +0500

    Таблицы orders и order_items

--- Marketplaes02/ViewModel/VewModelSostavZakaza.cs
+++ Marketplaes02/ViewModel/VewModelSostavZakaza.cs
@@ -1,6 +1,9 @@
-﻿using Marketplaes02.BD;
+﻿
+using Marketplaes02.BD;
 using Marketplaes02.Model;
+using Microsoft.Maui;
 using MySqlConnector;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text.Json;
 using System.Xml.Linq;
@@ -10,8 +13,10 @@ namespace Marketplaes02.ViewModel
 {
     public class VewModelSostavZakaza : OformlenieZakaza, INotifyPropertyChanged
     {
-
+        string Order_date;
+        int ID_order;
         int ID_goods;
+        int ID_user;
         private IList<SostavZakaza> _SostavZakazalist;
         public IList<SostavZakaza> SostavZakazalist
         {
@@ -33,11 +38,12 @@ namespace Marketplaes02.ViewModel
         public async void Load()
         {
 
-         
+            
             SostavZakazalist = GetList<SostavZakaza>("Sostavzakazalist");
             User_Name = Preferences.Default.Get("UserName", "NoName");
             User_Number_phone = Preferences.Default.Get("UserNumber_phone", "NoName");
             User_Adres_Dostavki = Preferences.Default.Get("UserAdres_Dostavki", "NoName");
+            ID_user = Preferences.Default.Get("UserID", 0);
             ButtonAdres_Dostavki = CheckUserAdres_Dostavki();
             User_Data = GetUser_Data();
             Goods_Total_Price_with_discount = GetGoods_Total_Price_with_discount();
@@ -46,20 +52,20 @@ namespace Marketplaes02.ViewModel
             //  await LoadSostavZakazalist(ID_goods);
         }
 
-        public async Task<bool> SQL_AddZakazi(int i)
+        public async Task<bool> SQL_AddOrders()
         {
             ConnectBD con = new ConnectBD();
-            
-            DateTime data_zakaza = DateTime.Now;
-            int UserID = Preferences.Default.Get("UserID", 0);
+
+            Order_date =  DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+           
 
             // int id_klienta = (Int32)Application.Current.Properties["UserID"];
-            var Status = "Заказ обрабатывается";
-            string sql = "INSERT INTO zakaz (`ID_user`, `ID_goods`, `Datetime_zakaza`, `Status`, `Total_Count`, `Total_Price_with_discount`) VALUES (@ID_user, @ID_goods, @Datetime_zakaza, @Status, @Total_Count, @Total_Price_with_discount)";
+            var Status = "В обработке";
+            string sql = "INSERT INTO `orders` (`ID_user`, `Order_date`, `Status`, `Total_Count`, `Total_Price_with_discount`) VALUES (@ID_user, @Order_date, @Status, @Total_Count, @Total_Price_with_discount)";
             MySqlCommand cmd = new MySqlCommand(sql, con.GetConnBD());
-            cmd.Parameters.Add(new MySqlParameter("@ID_user", UserID));
-            cmd.Parameters.Add(new MySqlParameter("@ID_goods", SostavZakazalist[i].ID_goods));
-            cmd.Parameters.Add(new MySqlParameter("@Datetime_zakaza", data_zakaza));
+            cmd.Parameters.Add(new MySqlParameter("@ID_user", ID_user));
+            // cmd.Parameters.Add(new MySqlParameter("@ID_goods", SostavZakazalist[i].ID_goods));
+            cmd.Parameters.Add(new MySqlParameter("@Order_date", Order_date));
             cmd.Parameters.Add(new MySqlParameter("@Status", Status));
             cmd.Parameters.Add(new MySqlParameter("@Total_Count", Goods_Total_Count));
             cmd.Parameters.Add(new MySqlParameter("@Total_Price_with_discount", Goods_Total_Price_with_discount));
@@ -68,14 +74,65 @@ namespace Marketplaes02.ViewModel
             await con.GetCloseBD();
             return true;
         }
+        private async Task<int> Get_ID_order(string Order_date, int ID_user)
+        {
+
+            string
+                sql = "SELECT * FROM orders WHERE Order_date=@Order_date AND ID_user=@ID_user";
+            ConnectBD con = new ConnectBD();
+
+            MySqlCommand
+             cmd = new MySqlCommand(sql, con.GetConnBD());
+            cmd.Parameters.Add(new MySqlParameter("@Order_date", Order_date));
+            cmd.Parameters.Add(new MySqlParameter("@ID_user", ID_user));
+
+            // Синхронное подключение к БД
+            await con.GetConnectBD();
+
+            // Объявление и инициалзиация метода асинрхонного чтения данных из бд
+            MySqlDataReader
+                 reader = cmd.ExecuteReader();
+            // Проверка, что строк нет
+            if (!reader.HasRows)
+            {
+                await con.GetCloseBD();
+            }
+            while (await reader.ReadAsync())
+            {
+                ID_order = Convert.ToInt32(reader["ID_order"]);
+
+            }
+            await con.GetCloseBD();
+
+            return ID_order;
+        }
+
+
+        public async Task<bool> SQL_AddOrder_items(int ID_order, int i)
+        {
+            ConnectBD con = new ConnectBD();
+          
+            string sql = "INSERT INTO `order_items` (`ID_order`, `ID_goods`, `Total_Price_with_discount`, `Total_Count`) VALUES (@ID_order, @ID_goods, @Total_Price_with_discount, @Total_Count)";
+            MySqlCommand cmd = new MySqlCommand(sql, con.GetConnBD());
+            cmd.Parameters.Add(new MySqlParameter("@ID_order", ID_order));
+            cmd.Parameters.Add(new MySqlParameter("@ID_goods", SostavZakazalist[i].ID_goods));
+            cmd.Parameters.Add(new MySqlParameter("@Total_Count", SostavZakazalist[i].Count));
+            cmd.Parameters.Add(new MySqlParameter("@Total_Price_with_discount", SostavZakazalist[i].Price_with_discount));
+            await con.GetConnectBD();
+            cmd.ExecuteNonQuery();
+            await con.GetCloseBD();
+            return true;
+        }
 
         public async Task<bool> AddZakazi()
         {
+            await SQL_AddOrders();
+            await Get_ID_order(Order_date, ID_user);
             int i = 0;
             for (; i < SostavZakazalist.Count; i++)
             {
               
-                await SQL_AddZakazi(i);
+                await SQL_AddOrder_items(ID_order, i);
           
 
             }
