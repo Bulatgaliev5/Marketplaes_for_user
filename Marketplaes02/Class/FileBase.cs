ddcd29d49cc1b53047f08b1b4841d60337c8d557
Admin <Admin@DESKTOP-V6V9F0T>
2024-06-12 13:11:56 +0500

    Версия первого релиза. В котором изображения не загружается

--- Marketplaes02/Class/FileBase.cs
+++ Marketplaes02/Class/FileBase.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using FluentFTP;
-using Microsoft.Maui.Controls;
+
 
 
 namespace Marketplaes02.Class
